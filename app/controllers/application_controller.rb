fd035991593fb120bde9e5306c273be151cddde9
fantasticlife <michael@smethur.st>
2025-10-10 11:10:37 +0100

    added design system crumb, site title and date and time display to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  
+  include LibraryDesign::Crumbs
+  
+  $SITE_TITLE = 'Procedure Bots'
+  
+  $DATE_DISPLAY_FORMAT = '%-d %B %Y'
+  
+  $DATETIME_DISPLAY_FORMAT = '%-d %B %Y @ %H:%M'
 end
